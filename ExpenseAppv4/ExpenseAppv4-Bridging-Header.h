c717aa6db2bf734b3e1ac89538c02265fec90c03
fruitslayr <FrancisYoung@Athena-2.local>
2015-01-11 23:23:31 +1100

    Commit To GitHub

---
+++ ExpenseAppv4/ExpenseAppv4-Bridging-Header.h
@@ -0,0 +1,25 @@
+//
+//  ExpenseAppv4-Bridging-Header.h
+//  ExpenseAppv4
+//
+//  Created by Francis Young on 30/12/2014.
+//  Copyright (c) 2014 Francis Young. All rights reserved.
+//
+
+#ifndef ExpenseAppv4_ExpenseAppv4_Bridging_Header_h
+#define ExpenseAppv4_ExpenseAppv4_Bridging_Header_h
+
+
+#endif
+
+#import "HRBrightnessCursor.h"
+#import "HRBrightnessSlider.h"
+#import "HRColorCursor.h"
+#import "HRColorCursor.h"
+#import "HRColorInfoView.h"
+#import "HRColorMapView.h"
+#import "HRColorPickerView.h"
+#import "HRHSVColorUtil.h"
+#import "UIImage+CoreGraphics.h"
+
+#import "MSCellAccessory.h"
