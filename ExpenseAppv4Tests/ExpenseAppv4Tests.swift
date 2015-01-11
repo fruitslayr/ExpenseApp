c14f2db23b132510724cc125b38d849aaa76dcfc
fruitslayr <FrancisYoung@Athena-2.local>
2015-01-11 23:23:53 +1100

    Commit to GitHub

---
+++ ExpenseAppv4Tests/ExpenseAppv4Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ExpenseAppv4Tests.swift
+//  ExpenseAppv4Tests
+//
+//  Created by Francis Young on 29/12/2014.
+//  Copyright (c) 2014 Francis Young. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ExpenseAppv4Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
